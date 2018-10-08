2130d4470bb127891de47d219db0a938d1d31cec
Lamoot Romain <lamoot@b11p18.fil.univ-lille1.fr>
2018-10-08 10:39:46 +0200

    ajout d'un nouveau composant pour le formulaire d'ajout d'un nouvel element photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CatFormComponent } from './cat-form/cat-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatFormComponent
   ],
   imports: [
     BrowserModule
